0184e3259ed5252be43c89e983397d2cbe7139f4
kimcno3 <kimcno3@naver.com>
2022-04-20 16:52:37 +0900

    MainController 파일 생성

---
+++ src/main/java/project/baseball/controller/MainController.java
@@ -0,0 +1,14 @@
+package project.baseball.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 주석문.
+ *
+ * */
+@RestController
+@RequestMapping("/game")
+public class MainController {
+
+}
