612b6624b468f17ffb9fb0f1153c41596e747eee
kimcno3 <kimcno3@naver.com>
2022-04-21 03:00:52 +0900

    Config 파일 생성

---
+++ src/main/java/project/baseball/config/GameConfig.java
@@ -0,0 +1,13 @@
+package project.baseball.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * .
+ **/
+
+@Configuration
+@ComponentScan(basePackages = "project.baseball")
+public class GameConfig {
+}
